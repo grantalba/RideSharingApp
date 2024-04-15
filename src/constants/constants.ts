2cedb8531972a6c6116593aa9197eccd6a7f1f17
Grant <grant@Grants-MacBook-Pro.local>
2024-04-16 02:23:58 +0800

    chore: organize folder structure

---
+++ src/constants/constants.ts
@@ -0,0 +1,31 @@
+export type OnboardingScreens = {
+  id: number;
+  image: any;
+  title: string;
+  desc: string;
+};
+
+const onboarding_screens: OnboardingScreens[] = [
+  {
+    id: 1,
+    image: require('../assets/images/splash_1.png'),
+    title: 'Search your trip easily',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus varius nulla, quis dictum augue.',
+  },
+  {
+    id: 2,
+    image: require('../assets/images/splash_2.png'),
+    title: 'Explore destinations and promotions',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus varius nulla, quis dictum augue.',
+  },
+  {
+    id: 3,
+    image: require('../assets/images/splash_3.png'),
+    title: 'Manage your trips',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus varius nulla, quis dictum augue.',
+  },
+];
+
+export default {
+  onboarding_screens,
+};
