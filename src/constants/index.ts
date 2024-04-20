d94adb274c41393e3c14277ead97957453c4d4a8
Grant <grant@Grants-MacBook-Pro.local>
2024-04-21 00:22:49 +0800

    fix: folder structure

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,4 @@
 import constants from './constants';
-import icons from './icons';
-import images from './images';
 import theme, {COLORS, SIZES, FONTS} from './theme';
 
-export {constants, icons, images, theme, COLORS, SIZES, FONTS};
+export {constants, theme, COLORS, SIZES, FONTS};
